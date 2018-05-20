800602bbad4efa38229a5a924754461d6830b4f3
Gilmar Francisco <gilmarfrancisco828@gmail.com>
2018-05-20 15:48:33 -0300

    GUI

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,17 @@
 
+import Controlador.Empresa;
+import Telas.TelaPrincipal;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
-
+        
         Empresa emp = new Empresa();
-
-        MenuPrincipal main_menu = new MenuPrincipal();
-
-        while (true) {
-            main_menu.exibir();
-            switch (main_menu.opcao()) {
-                case 1:
-                    //menu cadastrar
-                    MenuCadastro cadCliente = new MenuCadastro();
-                    cadCliente.exibir();
-                    cadCliente.escolha(emp);
-                    break;
-                case 2:
-                    //registrar compra
-                    MenuRegistrarCompra compra = new MenuRegistrarCompra();
-                    compra.exibir();
-                    compra.cadCompra(emp);
-                    break;
-                case 3:
-                    //relatorios
-                    MenuRelatorio relatorio = new MenuRelatorio();
-                    relatorio.exibir();
-                    relatorio.escolha(emp);
-                    break;
-                case 4:
-                    //salvar dados
-                    Empresa.salvar(emp);
-                    System.out.println("Dados salvos com sucesso!");
-                    break;
-                case 5:
-                    //carregar dados
-                    System.out.println("Dados carregados com sucesso!");
-                    Empresa.carregar(emp);
-                    break;
-                case 6:
-                    System.exit(0);
-            }
-        }
+        
+        TelaPrincipal telinhaPrincipal = new TelaPrincipal(emp);
+        telinhaPrincipal.setVisible(true);
     }
 
 }
